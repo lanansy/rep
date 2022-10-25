c9a7172e7f6d73dfe073bd17ca0f96426b4cba4a
lanansy <2795704030@qq.com>
2022-10-25 22:50:03 +0800

    join detach test

--- test.c
+++ test.c
@@ -1,9 +1,34 @@
 #include<stdio.h>
-#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<pthread.h>
+
+void *thread_job(void *arg){
+	pthread_t tid;
+	int err = pthread_detach(pthread_self());
+	if(err > 0){
+		printf("pthread_detach call failed:%s\n",strerror(err));
+	}else
+		printf("普通线程设置为分离态，参数:%ld\n",(long int)arg);
+	sleep(3);
+	pthread_exit((void*)8);
+}
 
 
 int main(){
-	int x = 0;
-	printf("%d\n",pow(2,3));
+	pthread_t tid;
+	pthread_create(&tid,NULL,thread_job,(void*)100);
+	int err;
+	void *reval = NULL;
+	//sleep(0);
+	err = pthread_join(tid,&reval);
+	printf("main err:%s\n",strerror(err));
+	if(err > 0){
+		printf("pthread_join call failed:%s\n",strerror(err));
+		exit(-1);
+	}else
+		printf("回收成功，退出码:%ld\n",(long int)reval);
+
 	return 0;
 }
